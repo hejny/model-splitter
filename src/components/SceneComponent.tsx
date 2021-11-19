49ed81fbaba0e4f15314eb1e9a7ec9d7ce1c7ba9
Pavol Hejny <me@pavolhejny.com>
2021-11-20 00:14:19 +0100

    Basic 3D scene

---
+++ src/components/SceneComponent.tsx
@@ -0,0 +1,84 @@
+import { Engine, EngineOptions, Scene, SceneOptions } from '@babylonjs/core';
+import React, { useEffect, useRef } from 'react';
+
+interface ISceneComponentProps
+    extends React.DetailedHTMLProps<
+        React.CanvasHTMLAttributes<HTMLCanvasElement>,
+        HTMLCanvasElement
+    > {
+    antialias?: boolean;
+    engineOptions?: EngineOptions;
+    adaptToDeviceRatio?: boolean;
+    sceneOptions?: SceneOptions;
+    onRender?: (scene: Scene) => void;
+    onSceneReady?: (scene: Scene) => void;
+}
+
+export function SceneComponent(props: ISceneComponentProps) {
+    const reactCanvas = useRef(null);
+    const {
+        antialias,
+        engineOptions,
+        adaptToDeviceRatio,
+        sceneOptions,
+        onRender,
+        onSceneReady,
+        ...rest
+    } = props;
+
+    useEffect(() => {
+        if (reactCanvas.current) {
+            const engine = new Engine(
+                reactCanvas.current,
+                antialias,
+                engineOptions,
+                adaptToDeviceRatio,
+            );
+            const scene = new Scene(engine, sceneOptions);
+            if (scene.isReady()) {
+                if (onSceneReady) {
+                    onSceneReady(scene);
+                }
+            } else {
+                scene.onReadyObservable.addOnce((scene) => {
+                    if (onSceneReady) {
+                        onSceneReady(scene);
+                    }
+                });
+            }
+
+            engine.runRenderLoop(() => {
+                if (typeof onRender === 'function') {
+                    onRender(scene);
+                }
+                scene.render();
+            });
+
+            const resize = () => {
+                scene.getEngine().resize();
+            };
+
+            if (window) {
+                window.addEventListener('resize', resize);
+            }
+
+            return () => {
+                scene.getEngine().dispose();
+
+                if (window) {
+                    window.removeEventListener('resize', resize);
+                }
+            };
+        }
+    }, [
+        reactCanvas,
+        adaptToDeviceRatio,
+        antialias,
+        engineOptions,
+        onRender,
+        onSceneReady,
+        sceneOptions,
+    ]);
+
+    return <canvas ref={reactCanvas} {...rest} />;
+}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,74 @@
+import {
+    FreeCamera,
+    HemisphericLight,
+    MeshBuilder,
+    Vector3,
+} from '@babylonjs/core';
 import React from 'react';
 import styled from 'styled-components';
+import { SceneComponent } from '../components/SceneComponent';
 
 export function MainPage() {
     return (
         <MainPageDiv>
             <h1>Model splitter</h1>
+
+            <SceneComponent
+                onSceneReady={(scene) => {
+                    // This creates and positions a free camera (non-mesh)
+                    var camera = new FreeCamera(
+                        'camera1',
+                        new Vector3(0, 5, -10),
+                        scene,
+                    );
+
+                    // This targets the camera to scene origin
+                    camera.setTarget(Vector3.Zero());
+
+                    const canvas = scene.getEngine().getRenderingCanvas();
+
+                    // This attaches the camera to the canvas
+                    camera.attachControl(canvas, true);
+
+                    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+                    var light = new HemisphericLight(
+                        'light',
+                        new Vector3(0, 1, 0),
+                        scene,
+                    );
+
+                    // Default intensity is 1. Let's dim the light a small amount
+                    light.intensity = 0.7;
+
+                    // Our built-in 'box' shape.
+                    const box = MeshBuilder.CreateBox(
+                        'box',
+                        { size: 2 },
+                        scene,
+                    );
+
+                    // Move the box upward 1/2 its height
+                    box.position.y = 1;
+
+                    // Our built-in 'ground' shape.
+                    MeshBuilder.CreateGround(
+                        'ground',
+                        { width: 6, height: 6 },
+                        scene,
+                    );
+                }}
+            />
         </MainPageDiv>
     );
 }
 
-const MainPageDiv = styled.div``;
+const MainPageDiv = styled.div`
+    canvas {
+        z-index: -1;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+    }
+`;
